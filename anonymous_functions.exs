5115d1155493e325de81ba7563579ea8dcae61d4
William Oliveira <sudowilliam@gmail.com>
2014-02-09 01:46:45 -0200

    pattern matching

--- anonymous_functions.exs
+++ anonymous_functions.exs
@@ -10,3 +10,8 @@ IO.puts sum.(10, 30) # 40
 hello = fn -> "hello" end
 
 IO.puts hello.() # "hello"
+
+# pattern matching
+swap = fn { a, b } -> { b, a } end
+
+result = swap.( { 10, 15 } ) # 15, 10
